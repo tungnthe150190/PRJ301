376b3d7015c07e967c57472b4b4d609d97ad64de
tung <tungnthe150190@fpt.edu.vn>
2021-10-22 18:32:16 +0700

    add model

--- ProjectPRJ/src/java/model/Building.java
+++ ProjectPRJ/src/java/model/Building.java
@@ -10,5 +10,32 @@ package model;
  * @author Tung
  */
 public class Building {
+    private int buildID;
+    private String name;
+    private int numberOfFloors;
+
+    public int getBuildID() {
+        return buildID;
+    }
+
+    public void setBuildID(int buildID) {
+        this.buildID = buildID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
     
 }
