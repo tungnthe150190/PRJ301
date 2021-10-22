b45b1786ba027fac60acf20937039b70763a5aff
tung <tungnthe150190@fpt.edu.vn>
2021-10-22 17:50:06 +0700

    dbcontext

--- ProjectPRJ/src/java/dal/DBContext.java
+++ ProjectPRJ/src/java/dal/DBContext.java
@@ -5,10 +5,30 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Tung
  */
 public class DBContext {
-    
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "sa";
+            String pass = "yeuthuy111";
+            String url = "jdbc:sqlserver://TUNG:1433;databaseName=PRJ301";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
